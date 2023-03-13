09c9658af4aa36ed9cf1a2f1bcc248b12e9021da
CodingShambles <victortheveloper@gmail.com>
2023-03-13 03:48:37 -0300

    Adding Log Management, Models and Service Extensions

---
+++ LibraryManagerAPI/Extensions/ServiceExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Contracts;
+using LoggerService;
+
+namespace LibraryManagerAPI.Extensions;
+
+public static class ServiceExtensions
+{
+    public static void ConfigureCors(this IServiceCollection services)
+    {
+        services.AddCors(options =>
+        {
+            options.AddPolicy("CorsPolicy",
+                builder => builder.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+        });
+    }
+
+    //Default methods of IIS Express configuration is pretty good because it's a very basic project
+    public static void ConfigureIISIntegration(this IServiceCollection services)
+    {
+        services.Configure<IISOptions>(options => { 
+        
+        });
+    }
+
+    public static void ConfigureLoggerService(this IServiceCollection services)
+    {
+        services.AddSingleton<ILoggerManager, LoggerManager>();
+    }
+}
